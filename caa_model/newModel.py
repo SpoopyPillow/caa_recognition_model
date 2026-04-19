4a0daf5fd4a4de251f7699d66d7fe5dbe3d107e9
Michael Hu <mbh326684@gmail.com>
2026-04-19 16:05:36 -0400

    compute gof of the model

--- caa_model/newModel.py
+++ caa_model/newModel.py
@@ -23,8 +23,8 @@ NR_SAMPLES = 10000  # number of trials to use for MC likelihood computation
 
 NR_QUANTILES = 10
 
-CAA_Params = namedtuple(
-    "CAA_Params",
+CAAParams = namedtuple(
+    "CAAParams",
     [
         "c",  # High confidence boundary
         "mu_t",  # Target drift
@@ -40,7 +40,7 @@ CAA_Params = namedtuple(
     ],
 )
 
-param_bounds = CAA_Params(
+param_bounds = CAAParams(
     c=(0.0, 2.0),
     mu_t=(0.0, 2.0),
     mu_l=(0.0, 1.0),
@@ -53,6 +53,111 @@ param_bounds = CAA_Params(
     sigma_z0=(EPS, 1.0),
     t0=(0.0, 0.5),
 )
+
+
+def compute_gof_all(model_params, quantiles=NR_QUANTILES, data=DATA, use_chisq=True):
+    """
+    Computes the overall goodness-of-fit of the model defined by model_params.
+    This is the sum of the NLL or chi-square statistics for the distribution
+    of responses to both the old and new words.
+    """
+
+    c_val, mu_t, mu_l, d, tc, r_off, z0_t, z0_l, dT, s_z0, t0 = model_params
+    # Confidence bins (High, Med, Low)
+    c_list = [c_val, 0]
+
+    target_params = (c_list, mu_t, d, tc, r_off, z0_t, dT, s_z0, t0)
+    old_data = [data.rem_hit.rt, data.know_hit.rt, data.miss.rt, data.rem_hit.conf, data.know_hit.conf]
+
+    lure_params = (c_list, mu_l, d, tc, r_off, z0_l, dT, s_z0, t0)
+    new_data = [data.rem_fa.rt, data.know_fa.rt, data.CR.rt, data.rem_fa.conf, data.know_fa.conf]
+
+    gof_target = compute_model_gof(target_params, *old_data, nr_quantiles=quantiles, use_chisq=use_chisq)
+    gof_lure = compute_model_gof(lure_params, *new_data, nr_quantiles=quantiles, use_chisq=use_chisq)
+
+    return gof_target + gof_lure
+
+
+def compute_model_gof(
+    model_params, rem_RTs, know_RTs, new_RTs, rem_conf, know_conf, nr_quantiles=NR_QUANTILES, use_chisq=True
+):
+    """
+    Compute the NLL or chi-square fit of the model to the data.
+    """
+
+    # Total number of trials N
+    N = len(rem_RTs) + len(know_RTs) + len(new_RTs)
+
+    # Predicted quantiles and total mass for each category
+    rem_qs, know_qs, new_qs, p_r, p_k, p_n = compute_model_quantiles(model_params, nr_quantiles)
+
+    # Number of confidence levels being used in the model
+    nr_conf_levels = len(rem_qs)
+
+    # Adjust the number of confidence levels in the data to match
+    rem_conf = pl.clip(rem_conf, 0, nr_conf_levels - 1)
+    know_conf = pl.clip(know_conf, 0, nr_conf_levels - 1)
+
+    # Computer number of RTs falling into each quantile bin
+    rem_freqs = pl.array(
+        [-pl.diff([pl.sum(rem_RTs[rem_conf == i] > q) for q in rem_qs[i]] + [0]) for i in range(nr_conf_levels)]
+    )
+    know_freqs = pl.array(
+        [-pl.diff([pl.sum(know_RTs[know_conf == i] > q) for q in know_qs[i]] + [0]) for i in range(nr_conf_levels)]
+    )
+    new_freqs = -pl.diff([pl.sum(new_RTs > q) for q in new_qs] + [0])
+
+    # Flip these frequencies so they are in order of descending confidence levels
+    rem_freqs = pl.flipud(rem_freqs)
+    know_freqs = pl.flipud(know_freqs)
+
+    x = pl.hstack([rem_freqs.flatten(), know_freqs.flatten(), new_freqs])
+
+    # Compute probabilities for each category (1/nr_quantiles of mass is in each bin)
+    p_rem_pred = p_r[:, None] * pl.ones((nr_conf_levels, nr_quantiles)) / float(nr_quantiles)
+    p_know_pred = p_k[:, None] * pl.ones((nr_conf_levels, nr_quantiles)) / float(nr_quantiles)
+    p_new_pred = p_n * pl.ones(nr_quantiles) / float(nr_quantiles)
+
+    p_pred = pl.hstack([p_rem_pred.flatten(), p_know_pred.flatten(), p_new_pred])
+
+    if use_chisq:
+        return chi_square_gof(x, N, p_pred)
+    else:
+        return -multinom_loglike(x, N, p_pred)
+
+
+def compute_model_quantiles(model_params, nr_quantiles=NR_QUANTILES):
+    """
+    Run the model and extract RT boundaries for each category.
+    """
+
+    quantile_inc = 1.0 / nr_quantiles
+    quantiles = pl.arange(0, 1, quantile_inc)
+
+    # Call the model
+    p_rem_conf, p_know_conf, p_new, t = predicted_proportions(*model_params)
+
+    # Calculate mass per confidence bin
+    p_rem_total = pl.sum(p_rem_conf, axis=-1) + EPS
+    p_know_total = pl.sum(p_know_conf, axis=-1) + EPS
+    p_new_total = pl.sum(p_new) + EPS
+
+    # Create CDF
+    P_rem = pl.cumsum(p_rem_conf, axis=-1) / p_rem_total[:, None]
+    P_know = pl.cumsum(p_know_conf, axis=-1) / p_know_total[:, None]
+    P_new = pl.cumsum(p_new) / p_new_total
+
+    # Find time point for each quantile
+    rem_quantiles = pl.array([t[pl.argmax(P_rem > q, -1)] for q in quantiles]).T
+    know_quantiles = pl.array([t[pl.argmax(P_know > q, -1)] for q in quantiles]).T
+    new_quantiles = pl.array([t[pl.argmax(P_new > q)] for q in quantiles])
+
+    # Initialize first quantile at t=0
+    rem_quantiles[:, 0] = 0
+    know_quantiles[:, 0] = 0
+    new_quantiles[0] = 0
+
+    return rem_quantiles, know_quantiles, new_quantiles, p_rem_total, p_know_total, p_new_total
 
 
 def predicted_proportions(c, mu_r, d, tc_bound, r_bound_offset, z0, deltaT, sigma_z0, t0=0):
